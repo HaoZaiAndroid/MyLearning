aef460f91fc7806bca2543354dc6399067a1d923
HaoZaiAndroid <yh_hzhx@163.com>
2018-05-15 22:03:46 +0800

    databinding test

--- app/src/main/java/com/learn/learning/MainActivity.java
+++ app/src/main/java/com/learn/learning/MainActivity.java
@@ -1,14 +1,25 @@
 package com.learn.learning;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+
+import com.learn.learning.databinding.ActivityMainBinding;
+import com.learn.learning.databinding.Person;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding mMainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+
+    public void testBinding() {
+        Person person = new Person();
+        person.setAge("18");
+        person.setBirthday("10.01");
+        person.setName("洪三元");
+        mMainBinding.setVariable(BR.person, person);
     }
 }
