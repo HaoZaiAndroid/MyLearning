aff4b08a5dc0dbbac5fe350425df902d3bc7744e
HaoZaiAndroid <yh_hzhx@163.com>
2018-06-12 22:11:11 +0800

    DrawerLayout沉浸式 解决方案

--- app/src/main/java/com/learn/learning/activity/MainActivity.java
+++ app/src/main/java/com/learn/learning/activity/MainActivity.java
@@ -1,16 +1,17 @@
 package com.learn.learning.activity;
 
 import android.databinding.DataBindingUtil;
-import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.learn.learning.R;
 import com.learn.learning.databinding.ActivityMainBinding;
+import com.learn.learning.utils.StatusBarUtil;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -21,35 +22,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        immersiveStatusBar();
         super.onCreate(savedInstanceState);
         mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         init();
 
+    }
+
+    private void initStatusView() {
+        ViewGroup.LayoutParams layoutParams = mainBinding.include.viewStatus.getLayoutParams();
+        layoutParams.height = StatusBarUtil.getStatusBarHeight(this);
+        mainBinding.include.viewStatus.setLayoutParams(layoutParams);
     }
     private void immersiveStatusBar() {
         if (Build.VERSION.SDK_INT >= 21) {
             View decorView = getWindow().getDecorView();
             int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
             decorView.setSystemUiVisibility(option);
-            getWindow().setStatusBarColor(Color.TRANSPARENT);
+           // getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
     }
 
     private void init() {
-        immersiveStatusBar();
         initViewID();
-        initDrawLayout();
+        StatusBarUtil.setTranslucentForDrawerLayout(this, mDrawerLayout, 0);
     }
-
     private void initViewID() {
         mNavView = mainBinding.navView;
         mDrawerLayout = mainBinding.drawerRoot;
-
     }
 
-    private void initDrawLayout() {
-        mNavView.inflateHeaderView(R.layout.nav_header_main);
-    }
 
 
     @Override
