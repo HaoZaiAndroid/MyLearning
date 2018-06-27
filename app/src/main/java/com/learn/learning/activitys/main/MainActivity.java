be2320a667b80a97226e17d04153e2378b025cfe
HaoZaiAndroid <yh_hzhx@163.com>
2018-06-27 21:56:28 +0800

    titile_bar 添加水波纹

--- app/src/main/java/com/learn/learning/activitys/main/MainActivity.java
+++ app/src/main/java/com/learn/learning/activitys/main/MainActivity.java
@@ -3,16 +3,19 @@ package com.learn.learning.activitys.main;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.learn.learning.R;
-import com.learn.learning.activitys.BaseDaggerActivity;
 import com.learn.learning.activitys.main.view.adapter.MainPagerAdapter;
 import com.learn.learning.activitys.main.view.fragment.TabOneFragment;
 import com.learn.learning.activitys.main.view.fragment.TabThreeFragment;
 import com.learn.learning.activitys.main.view.fragment.TabTwoFragment;
+import com.learn.learning.base.BaseDaggerActivity;
 import com.learn.learning.databinding.ActivityMainBinding;
 import com.learn.learning.utils.StatusBarUtil;
 
@@ -23,6 +26,10 @@ import javax.inject.Inject;
 
 
 public class MainActivity extends BaseDaggerActivity implements View.OnClickListener {
+    private static final int TAB_ONE_INDEX = 0;
+    private static final int TAB_TWO_INDEX = 1;
+    private static final int TAB_THREE_INDEX = 2;
+
     @Inject
     TabOneFragment tabOneFragment;
     @Inject
@@ -34,6 +41,11 @@ public class MainActivity extends BaseDaggerActivity implements View.OnClickList
 
     private ActivityMainBinding mainBinding;
     private ViewPager mainViewPager;
+    private ImageView ivTitleOne;
+    private ImageView ivTitleTwo;
+    private ImageView ivTitleThree;
+    private ImageView ivTitleMenu;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,15 +53,29 @@ public class MainActivity extends BaseDaggerActivity implements View.OnClickList
         mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         init();
     }
-
     private void init() {
         initId();
         initStatusView();
         initFragment();
+        initListener();
+    }
+    private void initListener() {
+        ivTitleOne.setOnClickListener(this);
+        ivTitleTwo.setOnClickListener(this);
+        ivTitleThree.setOnClickListener(this);
+        ivTitleMenu.setOnClickListener(this);
+
     }
 
+
     private void initId() {
+
+        ivTitleMenu = mainBinding.include.ivTitleMenu;
         mainViewPager = mainBinding.include.mainViewPager;
+        ivTitleOne = mainBinding.include.ivTitleOne;
+        ivTitleTwo = mainBinding.include.ivTitleTwo;
+        ivTitleThree = mainBinding.include.ivTitleThree;
+        drawerLayout = mainBinding.drawerRoot;
     }
 
     private void initFragment() {
@@ -71,8 +97,48 @@ public class MainActivity extends BaseDaggerActivity implements View.OnClickList
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.iv_title_one:
+                setCurrentItem(TAB_ONE_INDEX);
+                break;
+            case R.id.iv_title_two:
+                setCurrentItem(TAB_TWO_INDEX);
+                break;
+            case R.id.iv_title_three:
+                setCurrentItem(TAB_THREE_INDEX);
+                break;
+            case R.id.iv_title_menu:
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
             default:
                 break;
         }
+    }
+
+    /**
+     * 切换页面
+     * @param position 分类角标
+     */
+    private void setCurrentItem(int position) {
+        boolean isOne = false;
+        boolean isTwo = false;
+        boolean isThree = false;
+        switch (position) {
+            case 0:
+                isOne = true;
+                break;
+            case 1:
+                isTwo = true;
+                break;
+            case 2:
+                isThree = true;
+                break;
+            default:
+                isTwo = true;
+                break;
+        }
+        //vpContent.setCurrentItem(position);
+        ivTitleOne.setSelected(isOne);
+        ivTitleTwo.setSelected(isTwo);
+        ivTitleThree.setSelected(isThree);
     }
 }
--- app/src/main/java/com/learn/learning/base/BaseDaggerActivity.java
+++ app/src/main/java/com/learn/learning/base/BaseDaggerActivity.java
@@ -1,4 +1,4 @@
-package com.learn.learning.activitys;
+package com.learn.learning.base;
 
 import android.graphics.Color;
 import android.os.Build;
